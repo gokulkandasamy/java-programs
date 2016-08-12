b970f8b312397bc2eab6896efcae9573b5ba30d1
gokulkandasamy <gokulkandasamy03@gmail.com>
2016-08-12 10:06:12 +0530

    Add files via upload
    player2

---
+++ player2.java
@@ -0,0 +1,17 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class player2 {
+public static void main(String[] args) {
+	int f=1;
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	for(int i=1;i<=n;i++)
+	{
+		f=f*i;
+	}
+	System.out.println(f);
+	sc.close();
+}
+}
