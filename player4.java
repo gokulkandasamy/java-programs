52c2ebe6d8d5c5337c11a489aa638337844ff838
gokulkandasamy <gokulkandasamy03@gmail.com>
2016-08-12 10:07:19 +0530

    Add files via upload
    player4

---
+++ player4.java
@@ -0,0 +1,13 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class player4 {
+	public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+int n=Integer.parseInt(s);
+System.out.println(n);
+sc.close();
+	}
+}
